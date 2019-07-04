78fd673d342a835b19ab2e4937c5a6686cf574df
Jay Palat <jay@palat.net>
2019-07-04 16:23:12 -0400

    Move to my data source.

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/15k5RkFKoFZW5r-mNGnni1N_1bYh7XJ-7OqI_jse2veE/edit#gid=0';
